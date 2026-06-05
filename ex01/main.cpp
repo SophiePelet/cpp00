5c21b11e2d7bc9184026eccbf4f7b0c300c6af39
Sophie <peletsophie@gmail.com>
2026-06-05 16:38:14 +0200

    feat: update the `ADD` command
    - Creates `addCommand()` to shorten the main
    - Returns an error if the phone number is more/less than 10 characters
    or if it contains a char that's not a digit

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,12 +6,55 @@
 /*   By: sopelet <sopelet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 14:41:58 by sopelet           #+#    #+#             */
-/*   Updated: 2026/06/03 16:35:29 by sopelet          ###   ########.fr       */
+/*   Updated: 2026/06/05 16:35:51 by sopelet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 #include "Contact.hpp"
+
+
+/* 
+	Checks if the format of the phone number is correct
+	- must be 10 characters long
+	- nust contain digits only
+*/
+int	ft_isnum(std::string phone_number) {
+	for (size_t i = 0; i < phone_number.length(); ++i) {
+		if (phone_number[i] < '0' || phone_number[i] > '9')
+			return (std::cerr << "Unauthorized character in phone number" << std::endl, 0);
+	if (phone_number.length() != 10)
+		return (std::cerr << "Phone number must be 10 characters" << std::endl, 0);
+	}
+	return (1);
+}
+
+/*
+	Used when the user calls `ADD`
+	Allows the user to enter the needed contact informations
+*/
+void	addCommand(PhoneBook myPhonebook) {
+	std::string	first_name, last_name, nickname, darkest_secret, phone_number;
+	Contact	temp;
+
+	std::cout << "Enter contact first name: " << std::endl;
+	std::cin >> first_name;
+	std::cout << "Enter contact last name: " << std::endl;
+	std::cin >> last_name;
+	std::cout << "Enter contact nickname: " << std::endl;
+	std::cin >> nickname;
+	std::cout << "Enter contact darkest secret: " << std::endl;
+	std::cin >> darkest_secret;
+	std::cout << "Enter phone number: " << std::endl;
+	std::cin >> phone_number;
+	while (!ft_isnum(phone_number)) {
+		std::cout << "Enter phone number: " << std::endl;
+		std::cin >> phone_number;
+	}
+	temp.setFields(first_name, last_name, nickname, phone_number, darkest_secret);
+	myPhonebook.addContact(temp);
+	std::cout << "Contact successfully added" << std::endl;
+}
 
 int main(void) {
 	PhoneBook	myPhonebook;
@@ -20,23 +63,8 @@ int main(void) {
 	while (1) {
 		std::cout << "What do you want to do? " << std::endl;
 		std::cin >> command;
-		if (command == "ADD") {
-			std::string	first_name, last_name, nickname, darkest_secret, phone_number;
-			Contact	temp;
-
-			std::cout << "Enter contact first name: " << std::endl;
-			std::cin >> first_name;
-			std::cout << "Enter contact last name: " << std::endl;
-			std::cin >> last_name;
-			std::cout << "Enter contact nickname: " << std::endl;
-			std::cin >> nickname;
-			std::cout << "Enter contact darkest secret: " << std::endl;
-			std::cin >> darkest_secret;
-			std::cout << "Enter phone number: " << std::endl;
-			std::cin >> phone_number;
-			temp.setFields(first_name, last_name, nickname, phone_number, darkest_secret);
-			myPhonebook.addContact(temp);
-		}
+		if (command == "ADD")
+			addCommand(myPhonebook);
 		else if (command == "SEARCH") {
 			std::cout << "Loading the contact informations..." << std::endl;
 			// Searching logic
@@ -45,9 +73,8 @@ int main(void) {
 			std::cout << "Closing the phonebook..." << std::endl;
 			return (0);
 		}
-		else {
+		else
 			std::cout << "Unknown command, please try again" << std::endl;
-		}
 	}
 	return (0);
 }
